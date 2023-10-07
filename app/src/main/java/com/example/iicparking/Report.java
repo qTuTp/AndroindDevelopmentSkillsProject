6080bc2fa90fc24878696a171b38c743d05d1914
KaiMinBong <p21013393@student.newinti.edu.my>
2023-10-07 20:47:52 +0800

    added a new layout called profile edit to allows users to edit their profile information. (The layout still needs some touch-up to make it more neat)

--- app/src/main/java/com/example/iicparking/Report.java
+++ app/src/main/java/com/example/iicparking/Report.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class report extends AppCompatActivity {
+public class Report extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
