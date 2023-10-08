12c63da1170d034a6924a67a398bf2a1331c44b5
Chan Huanyi <chyafs@gmail.com>
2023-10-08 16:39:42 +0800

    Add the animation and function to transit in days for the peak time page (Non-Tested)

--- app/src/main/java/com/example/iicparking/PeakTimePage.java
+++ app/src/main/java/com/example/iicparking/PeakTimePage.java
@@ -3,6 +3,8 @@ package com.example.iicparking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -14,15 +16,15 @@ public class PeakTimePage extends AppCompatActivity {
     private TextView primaryDay;
     private TextView priorDay;
     private TextView afterDay;
-    private final String[] DAYS = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    private Day currentDay;
-    private Day previousDay;
-    private Day laterDay;
+
+    private int currentDay;
+    private String[] DAYS;
+
     private enum Day {
         SUNDAY, MONDAY, TUESDAY, WEDNESDAY,THURSDAY, FRIDAY, SATURDAY;
 
         public Day toPreviousDay(){
-            return values()[(ordinal() - 1) % 7 ];
+            return values()[(ordinal() - 1 + 7) % 7 ];
         }
 
         public Day toNextDay(){
@@ -36,6 +38,7 @@ public class PeakTimePage extends AppCompatActivity {
         setContentView(R.layout.activity_peak_time_page);
         setReference();
         getToday();
+        updateDay(0);
 
     }
 
@@ -43,6 +46,8 @@ public class PeakTimePage extends AppCompatActivity {
         primaryDay = findViewById(R.id.primaryDay);
         priorDay = findViewById(R.id.priorDay);
         afterDay = findViewById(R.id.afterDay);
+        DAYS = getResources().getStringArray(R.array.days_of_week);
+
 
         //indicator 1 mean prior day is clicked, while 2 indicate after day is clicked
         priorDay.setOnClickListener(v -> updateDay(1));
@@ -55,20 +60,94 @@ public class PeakTimePage extends AppCompatActivity {
             case 0: //Initialize layout
                 break;
             case 1: //If prior day, move the primary day to the previous day
-
+                currentDay = (currentDay - 1 + 7) % 7;
                 break;
             case 2: //If after day, move the primary day to the day after
+                currentDay = (currentDay + 1 + 7) % 7;
                 break;
+
         }
+
+        Day today = Day.values()[currentDay];
+        animateTextUpdate(today);
+    }
+
+    private void animateTextUpdate(Day today){
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+
+
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+                //The text won't be able to be click
+                priorDay.setClickable(false);
+                primaryDay.setClickable(false);
+                afterDay.setClickable(false);
+                priorDay.setFocusable(false);
+                primaryDay.setFocusable(false);
+                afterDay.setFocusable(false);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //Update Text
+                priorDay.setText(DAYS[today.toPreviousDay().ordinal()]);
+                primaryDay.setText(DAYS[today.ordinal()]);
+                afterDay.setText(DAYS[today.toNextDay().ordinal()]);
+
+                //Fade in
+                priorDay.startAnimation(fadeIn);
+                primaryDay.startAnimation(fadeIn);
+                afterDay.startAnimation(fadeIn);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //Restore the clickable and focusable
+                priorDay.setClickable(true);
+                primaryDay.setClickable(true);
+                afterDay.setClickable(true);
+                priorDay.setFocusable(true);
+                primaryDay.setFocusable(true);
+                afterDay.setFocusable(true);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        //Fade out
+        priorDay.startAnimation(fadeOut);
+        primaryDay.startAnimation(fadeOut);
+        afterDay.startAnimation(fadeOut);
+
     }
 
     //To get the current date
     private void getToday(){
         Calendar calendar = Calendar.getInstance();
-        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        currentDay = calendar.get(Calendar.DAY_OF_WEEK) - 1;
 
-        currentDay = Day.values()[day - 1];
-        previousDay = currentDay.toPreviousDay();
-        laterDay = currentDay.toPreviousDay();
+
     }
 }
