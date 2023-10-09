9b00a49f163469ca59ff11ee74208824444483cb
Chan Huanyi <chyafs@gmail.com>
2023-10-09 20:42:13 +0800

    Add button on main activity which allow direct access to page, easier for testing

--- app/src/main/java/com/example/iicparking/MainActivity.java
+++ app/src/main/java/com/example/iicparking/MainActivity.java
@@ -1,18 +1,53 @@
 package com.example.iicparking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton loginP;
+    private AppCompatButton regisP;
+    private AppCompatButton homeP;
+    private AppCompatButton peakP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, LoginPage.class);
-        startActivity(intent);
+        setReference();
+
+
+    }
+
+    private void setReference(){
+        loginP = findViewById(R.id.loginP);
+        regisP = findViewById(R.id.registerP);
+        homeP = findViewById(R.id.homeP);
+        peakP = findViewById(R.id.peakP);
+
+        loginP.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginPage.class);
+            startActivity(intent);
+        });
+
+        regisP.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegisterPage.class);
+            startActivity(intent);
+        });
+
+        homeP.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, HomePage.class);
+            startActivity(intent);
+        });
+
+        peakP.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PeakTimePage.class);
+            startActivity(intent);
+        });
 
 
     }
