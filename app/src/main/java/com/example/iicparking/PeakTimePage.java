e4f1d2673b7195ca9accc788d0ece76c30363851
Chan Huanyi <chyafs@gmail.com>
2023-10-09 23:04:15 +0800

    Add graph to peak time page

--- app/src/main/java/com/example/iicparking/PeakTimePage.java
+++ app/src/main/java/com/example/iicparking/PeakTimePage.java
@@ -1,19 +1,45 @@
 package com.example.iicparking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class PeakTimePage extends AppCompatActivity {
+
+    private class HourAxisValueFormatter extends ValueFormatter {
+
+        @Override
+        public String getFormattedValue(float value) {
+            int hour = (int) value;
+            return String.format(Locale.ENGLISH, "%02d:00", hour);
+        }
+
+    }
 
     private TextView primaryDay;
     private TextView priorDay;
     private TextView afterDay;
+    private HorizontalBarChart peakTimeChart;
 
     private int currentDay;
     private String[] DAYS;
@@ -37,6 +63,7 @@ public class PeakTimePage extends AppCompatActivity {
         setReference();
         getToday();
         updateDay(0);
+        updateChart();
 
     }
 
@@ -45,6 +72,7 @@ public class PeakTimePage extends AppCompatActivity {
         priorDay = findViewById(R.id.priorDay);
         afterDay = findViewById(R.id.afterDay);
         DAYS = getResources().getStringArray(R.array.days_of_week);
+        peakTimeChart = findViewById(R.id.peakTimeChart);
 
 
         //indicator 1 mean prior day is clicked, while 2 indicate after day is clicked
@@ -56,6 +84,10 @@ public class PeakTimePage extends AppCompatActivity {
 
         switch (indicator){
             case 0: //Initialize layout
+                //Set the visibility to invisible
+                priorDay.setVisibility(View.INVISIBLE);
+                primaryDay.setVisibility(View.INVISIBLE);
+                afterDay.setVisibility(View.INVISIBLE);
                 break;
             case 1: //If prior day, move the primary day to the previous day
                 currentDay = (currentDay - 1 + 7) % 7;
@@ -92,6 +124,11 @@ public class PeakTimePage extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
+                //Set the visibility to invisible
+                priorDay.setVisibility(View.INVISIBLE);
+                primaryDay.setVisibility(View.INVISIBLE);
+                afterDay.setVisibility(View.INVISIBLE);
+
                 //Update Text
                 priorDay.setText(DAYS[today.toPreviousDay().ordinal()]);
                 primaryDay.setText(DAYS[today.ordinal()]);
@@ -118,6 +155,11 @@ public class PeakTimePage extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
+                //Set the visibility to visible
+                priorDay.setVisibility(View.VISIBLE);
+                primaryDay.setVisibility(View.VISIBLE);
+                afterDay.setVisibility(View.VISIBLE);
+
                 //Restore the clickable and focusable
                 priorDay.setClickable(true);
                 primaryDay.setClickable(true);
@@ -148,4 +190,49 @@ public class PeakTimePage extends AppCompatActivity {
 
 
     }
+
+    //Update the visual of the chart
+    private void updateChart(){
+
+
+        List<BarEntry> values = new ArrayList<>();
+
+        // Suppose you're collecting data every hour over 24 hours.
+        for (int hour = 0; hour < 24; hour++) {
+            float randomCarsCount = (float) (Math.random() * 500); // Random number of cars
+            values.add(new BarEntry(hour, randomCarsCount));
+        }
+
+
+
+        List<Integer> colors = new ArrayList<>();
+        for (BarEntry entry : values) {
+            colors.add(getColorForEntry(entry));
+        }
+
+        BarDataSet set = new BarDataSet(values, "Number of Cars");
+        set.setColors(colors);
+
+        XAxis xAxis = peakTimeChart.getXAxis();
+        xAxis.setValueFormatter(new HourAxisValueFormatter());
+
+        BarData barData = new BarData(set);
+        peakTimeChart.setData(barData);
+        peakTimeChart.invalidate();
+
+
+    }
+
+    private int getColorForEntry(BarEntry entry) {
+        if (entry.getY() > 400) {
+            return Color.RED;
+        } else if (entry.getY() > 250) {
+            return Color.YELLOW;
+        } else {
+            return Color.GREEN;
+        }
+    }
+
 }
+
+
