6ca29916bf559ce01b9fa966aa121619471d2b22
KaiMinBong <p21013393@student.newinti.edu.my>
2023-11-18 22:07:15 +0800

    created the push notification layout and added firebase cloud messaging service.

---
+++ app/src/main/java/com/example/iicparking/MyFirebaseMessagingService.java
@@ -0,0 +1,63 @@
+package com.example.iicparking;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViews;
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        if (remoteMessage.getNotification() != null){
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private RemoteViews getCustomDesign(String title, String message) {
+        RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.push_notification);
+        remoteViews.setTextViewText(R.id.app_title, title);
+        remoteViews.setTextViewText(R.id.app_message, message);
+        remoteViews.setImageViewResource(R.id.app_logo, R.drawable.inti_star_logo);
+        return remoteViews;
+    }
+
+    public void showNotification(String title, String message) {
+        Intent intent = new Intent(this, NotificationListPage.class);
+        String channel_id = "notification_channel";
+        String channel_name = "com.example.iicparking";
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channel_id)
+                .setSmallIcon(R.drawable.inti_star_logo)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000})
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            builder = builder.setContent(getCustomDesign(title, message));
+        } else {
+            builder = builder.setContentTitle(title)
+                    .setContentText(message)
+                    .setSmallIcon(R.drawable.inti_star_logo);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id, channel_name, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        notificationManager.notify(0,builder.build());
+
+    }
+}
+
